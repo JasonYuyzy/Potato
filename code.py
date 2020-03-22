37d3bf3bb44ade62a87170815e1fe76fdc634db0
YDD <zhengyu.yu2@durham.ac.uk>
2020-03-22 20:57:24 +0000

    example of the code

---
+++ code.py
@@ -0,0 +1,130 @@
+import sys
+import urllib.request
+
+assert sys.version_info >= (3, 5)
+# Scikit-Learn ≥0.20 is required
+import sklearn
+assert sklearn.__version__ >= "0.20"
+
+def prepare_country_stats(oecd_bli, gdp_per_capita):
+    oecd_bli = oecd_bli[oecd_bli["INEQUALITY"]=="TOT"]
+    oecd_bli = oecd_bli.pivot(index="Country", columns="Indicator", values="Value")
+    gdp_per_capita.rename(columns={"2015": "GDP per capita"}, inplace=True)
+    gdp_per_capita.set_index("Country", inplace=True)
+    full_country_stats = pd.merge(left=oecd_bli, right=gdp_per_capita,
+                                  left_index=True, right_index=True)
+    full_country_stats.sort_values(by="GDP per capita", inplace=True)
+    remove_indices = [0, 1, 6, 8, 33, 34, 35]
+    keep_indices = list(set(range(36)) - set(remove_indices))
+    return full_country_stats[["GDP per capita", 'Life satisfaction']].iloc[keep_indices]
+
+import os
+datapath = os.path.join("datasets", "lifesat", "")
+
+# To plot pretty figures directly within Jupyter
+#%matplotlib inline
+import matplotlib as mpl
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+# Download the data
+import urllib.request
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
+os.makedirs(datapath, exist_ok=True)
+for filename in ("oecd_bli_2015.csv", "gdp_per_capita.csv"):
+    print("Downloading", filename)
+    url = DOWNLOAD_ROOT + "datasets/lifesat/" + filename
+    urllib.request.urlretrieve(url, datapath + filename)
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sklearn.linear_model
+
+# Load the data
+oecd_bli = pd.read_csv(datapath + "oecd_bli_2015.csv", thousands=',')
+gdp_per_capita = pd.read_csv(datapath + "gdp_per_capita.csv",thousands=',',delimiter='\t',encoding='latin1', na_values="n/a")
+
+# Prepare the data
+country_stats = prepare_country_stats(oecd_bli, gdp_per_capita)
+X = np.c_[country_stats["GDP per capita"]]
+y = np.c_[country_stats["Life satisfaction"]]
+
+# Visualize the data
+country_stats.plot(kind='scatter', x="GDP per capita", y='Life satisfaction')
+plt.show()
+
+# Select a linear model
+model = sklearn.linear_model.LinearRegression()
+
+# Train the model
+model.fit(X, y)
+
+# Make a prediction for Cyprus
+X_new = [[22587]]  # Cyprus' GDP per capita
+print(model.predict(X_new)) # outputs [[ 5.96242338]]
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "fundamentals"
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
+os.makedirs(IMAGES_PATH, exist_ok=True)
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+
+np.random.seed(42)
+
+oecd_bli = pd.read_csv(datapath + "oecd_bli_2015.csv", thousands=',')
+oecd_bli = oecd_bli[oecd_bli["INEQUALITY"]=="TOT"]
+oecd_bli = oecd_bli.pivot(index="Country", columns="Indicator", values="Value")
+oecd_bli.head(2)
+
+oecd_bli["Life satisfaction"].head()
+
+gdp_per_capita = pd.read_csv(datapath+"gdp_per_capita.csv", thousands=',', delimiter='\t',
+                             encoding='latin1', na_values="n/a")
+gdp_per_capita.rename(columns={"2015": "GDP per capita"}, inplace=True)
+gdp_per_capita.set_index("Country", inplace=True)
+gdp_per_capita.head(2)
+
+full_country_stats = pd.merge(left=oecd_bli, right=gdp_per_capita, left_index=True, right_index=True)
+full_country_stats.sort_values(by="GDP per capita", inplace=True)
+full_country_stats
+
+full_country_stats[["GDP per capita", 'Life satisfaction']].loc["United States"]
+
+remove_indices = [0, 1, 6, 8, 33, 34, 35]
+keep_indices = list(set(range(36)) - set(remove_indices))
+
+sample_data = full_country_stats[["GDP per capita", 'Life satisfaction']].iloc[keep_indices]
+missing_data = full_country_stats[["GDP per capita", 'Life satisfaction']].iloc[remove_indices]
+
+sample_data.plot(kind='scatter', x="GDP per capita", y='Life satisfaction', figsize=(5,3))
+plt.axis([0, 60000, 0, 10])
+position_text = {
+    "Hungary": (5000, 1),
+    "Korea": (18000, 1.7),
+    "France": (29000, 2.4),
+    "Australia": (40000, 3.0),
+    "United States": (52000, 3.8),
+}
+for country, pos_text in position_text.items():
+    pos_data_x, pos_data_y = sample_data.loc[country]
+    country = "U.S." if country == "United States" else country
+    plt.annotate(country, xy=(pos_data_x, pos_data_y), xytext=pos_text,
+            arrowprops=dict(facecolor='black', width=0.5, shrink=0.1, headwidth=5))
+    plt.plot(pos_data_x, pos_data_y, "ro")
+plt.xlabel("GDP per capita (USD)")
+save_fig('money_happy_scatterplot')
+plt.show()
+
+sample_data.to_csv(os.path.join("datasets", "lifesat", "lifesat.csv"))
+
+sample_data.loc[list(position_text.keys())]
