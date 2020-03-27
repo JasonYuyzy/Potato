94bb61113e5ea97856c16d28c709c656bbfe6d25
YDD <zhengyu.yu2@durham.ac.uk>
2020-03-27 00:58:51 +0000

    complete the download data and store the figure

--- classifier.py
+++ classifier.py
@@ -20,10 +20,33 @@ mpl.rc('axes', labelsize=14)
 mpl.rc('xtick', labelsize=12)
 mpl.rc('ytick', labelsize=12)
 
+#Where to save the CSV files
+DATA_PATH = os.path.join("datasets", "students_data")
+
+import zipfile
+import urllib.request
+DOWNLOAD_ROOT = "https://analyse.kmi.open.ac.uk/open_dataset/download"
+if not os.path.isdir(DATA_PATH):
+    os.makedirs(DATA_PATH)
+tgz_path = os.path.join(DATA_PATH, "student.zip")
+urllib.request.urlretrieve(DOWNLOAD_ROOT, tgz_path)
+#tarfile.getnames(tgz_path)
+student_file = zipfile.ZipFile(tgz_path)
+student_file.extractall(path=DATA_PATH)
+student_file.close()
+
+'''
+
+for filename in ("studentInfo.csv", "courses.csv"):
+    print("Downloading", filename)
+    # + "datasets/lifesat/" add if needed
+    url = DOWNLOAD_ROOT + filename
+'''
+
+print("finished")
+exit(0)
 # Where to save the figures
-PROJECT_ROOT_DIR = "."
-DATA_FIGURE_FOLDER = "data_figures"
-IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", DATA_FIGURE_FOLDER)
+IMAGES_PATH = os.path.join(".", "images", "data_figures")
 os.makedirs(IMAGES_PATH, exist_ok=True)
 
 def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
@@ -40,7 +63,10 @@ warnings.filterwarnings(action="ignore", message="^internal gelsd")
 #get the data
 import os
 import tarfile
+
+STUDENT_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
 STUDENT_PATH = os.path.join("datasets", "student")
+STUDENT_URL = STUDENT_ROOT + "datasets/housing/housing.tgz"
 
 #if not os.path.isdir(STUDENT_PATH):
         #os.makedirs(STUDENT_PATH)
@@ -52,19 +78,34 @@ def load_student_data(student_path=STUDENT_PATH):
 
 student = load_student_data()
 #show the data
-head = student.head()
-for h in head:
-    if h == "id_student" or h == "region" or h == "imd_band" or h == "num_of_prev_attempts":
-        continue
-    student[h].hist(bins=20, figsize=(8,8))
-    save_fig(h)
-    plt.close()
+def create_figure (data):
+    head = data.head()
+    for h in head:
+        if h == "id_student" or h == "region" or h == "imd_band" or h == "num_of_prev_attempts":
+            continue
+        student[h].hist(bins=20, figsize=(8,8))
+        save_fig(h)
+        plt.close()
 #print("head", head)
 #student.info()
-a = student["code_module"].value_counts()
-des = student.describe()
-print(des)
-import matplotlib.pyplot as plt
-student.hist(bins=50, figsize=(20,15))
+#create_figure(student)
+#a = student["code_module"].value_counts()
+#des = student.describe()
 #save_fig("studentInfo")
-plt.show()
+#plt.show()
+
+np.random.seed(42)
+import numpy as np
+test_ratio = 0.2
+def split_set(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+train_set, test_set = split_set(student, test_ratio)
+print(train_set)
+print(test_set)
+print(len(train_set))
+print(len(test_set))
